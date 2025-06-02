dafbf5d4088d12987e59b267f9225d37f9d3591a
SushantDahal10 <118923135+SushantDahal10@users.noreply.github.com>
2025-06-02 22:32:16 +0530

    Update connection.js

--- Backend/connection.js
+++ Backend/connection.js
@@ -1,17 +1,18 @@
-const sql=require('mysql2')
-const connection=sql.createConnection({
-    host:'localhost',
-    user:'root',
-    password:'12345',
-    database:'busproject',
+const sql = require('mysql2');
+const connection = sql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     timezone: '+00:00'
-})
+});
+
 connection.connect((error) => {
     if (error) {
-      console.error('Error connecting `to` the database:', error);
-      return;
+        console.error('Error connecting to the database:', error);
+        return;
     }
     console.log('Connected to the MySQL database.');
-  });
+});
 
-module.exports=connection;
+module.exports = connection;
